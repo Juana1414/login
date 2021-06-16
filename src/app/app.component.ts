442ecfb24acd64757d988795f7d6a1b4420b6e01
juanita pech <juanitapechhernandez38@gmail.com>
2021-06-15 21:10:45 -0500

    Feat Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css',
+  'static/vendor/bootstrap/css/bootstrap.min.css',
+  'static/fonts/font-awesome-4.7.0/css/font-awesome.min.css',
+  'static/fonts/Linearicons-Free-v1.0.0/icon-font.min.css',
+
+ 'static/vendor/animate/animate.css',
+
+ 'static/vendor/css-hamburgers/hamburgers.min.css',
+
+ 'static/vendor/animsition/css/animsition.min.css',
+
+  'static/vendor/select2/select2.min.css',
+ 'static/vendor/daterangepicker/daterangepicker.css',
+
+  'static/css/util.css',
+  'static/css/main.css',
+
+  ]
+
+
+})
+export class AppComponent {
+  title = 'login';
+}
